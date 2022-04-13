dd61537a9c57904b71f8cad45cba9425c86c9c1f
Hasham <hashamrehmat@gmail.com>
2022-04-13 12:18:02 +0500

    Updates

---
+++ precache-manifest.da6fe49a5d90055593ba96d95f9e1a7b.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e883afbde1783b0f681",
+    "url": "/comrades/static/css/main.fe624cb6.chunk.css"
+  },
+  {
+    "revision": "2e883afbde1783b0f681",
+    "url": "/comrades/static/js/main.2e883afb.chunk.js"
+  },
+  {
+    "revision": "e0951f3e1edda8b7f479",
+    "url": "/comrades/static/js/1.e0951f3e.chunk.js"
+  },
+  {
+    "revision": "ae61b831b5c0f34eb05c",
+    "url": "/comrades/static/js/2.ae61b831.chunk.js"
+  },
+  {
+    "revision": "a962dce7167311198a2f",
+    "url": "/comrades/static/js/runtime~main.a962dce7.js"
+  },
+  {
+    "revision": "7b1cedd0f4ee2b04cd1cd82bbcb860cd",
+    "url": "/comrades/static/media/counter-bg.7b1cedd0.jpg"
+  },
+  {
+    "revision": "0876dd7c0d046ecf63cef4302ac0f687",
+    "url": "/comrades/static/media/services-bg.0876dd7c.jpg"
+  },
+  {
+    "revision": "d33d35027efea781cb28e51a62dff456",
+    "url": "/comrades/static/media/HomeSlider22.d33d3502.jpg"
+  },
+  {
+    "revision": "0b452413f60facecb10624c543a0e72b",
+    "url": "/comrades/static/media/company.0b452413.png"
+  },
+  {
+    "revision": "603d5fe753a0c7b6722a4b1a6df35cb0",
+    "url": "/comrades/static/media/service-bg.603d5fe7.jpg"
+  },
+  {
+    "revision": "ee71f72ccdfa4205cf2bfa17e4eef433",
+    "url": "/comrades/static/media/about-bg.ee71f72c.jpg"
+  },
+  {
+    "revision": "450f54c36d05ec1fd9954b4d74818466",
+    "url": "/comrades/static/media/contact-bg.450f54c3.jpg"
+  },
+  {
+    "revision": "dd0bd974be9072c390d576642ab753ad",
+    "url": "/comrades/static/media/portfolio-bg.dd0bd974.jpg"
+  },
+  {
+    "revision": "db6b63cbbc08019b524dda3d89201697",
+    "url": "/comrades/static/media/team-bg.db6b63cb.jpg"
+  },
+  {
+    "revision": "cf2090ba78e7aa7e35aecb031d3ba6e3",
+    "url": "/comrades/static/media/service1.cf2090ba.jpg"
+  },
+  {
+    "revision": "8cc147a01d1106872de353b06dcad205",
+    "url": "/comrades/static/media/service2.8cc147a0.jpg"
+  },
+  {
+    "revision": "6da1724f65c3ab7513d5721c7423ab89",
+    "url": "/comrades/static/media/service3.6da1724f.jpg"
+  },
+  {
+    "revision": "3cd5092c019ee24588d307fa44c0e7cf",
+    "url": "/comrades/static/media/service4.3cd5092c.jpg"
+  },
+  {
+    "revision": "79111267edae3f4e71bdb7965c65995f",
+    "url": "/comrades/static/media/pro (2).79111267.jpg"
+  },
+  {
+    "revision": "f9f901e2ac80de2a2eea194388a671e7",
+    "url": "/comrades/static/media/pro (3).f9f901e2.jpg"
+  },
+  {
+    "revision": "badf757da3de6e6a888c33c0cddfd4b0",
+    "url": "/comrades/static/media/pro (4).badf757d.jpg"
+  },
+  {
+    "revision": "2ce96713c23b9f08429bfa73156eb11a",
+    "url": "/comrades/static/media/pro (5).2ce96713.jpg"
+  },
+  {
+    "revision": "ef298bd85d125d9425db4a7b821cd12d",
+    "url": "/comrades/static/media/pro (6).ef298bd8.jpg"
+  },
+  {
+    "revision": "4149c5c5f1f6606082cc880906260f88",
+    "url": "/comrades/static/media/pro (7).4149c5c5.jpg"
+  },
+  {
+    "revision": "e690a46bf07bf35bdc08d6c6b797620c",
+    "url": "/comrades/static/media/logo.e690a46b.png"
+  },
+  {
+    "revision": "fc7a02bfd86ebdbca8d1d5524d6aecc0",
+    "url": "/comrades/static/media/about1.fc7a02bf.jpg"
+  },
+  {
+    "revision": "ff2ad42f1649c87a675a04c7ac7828d6",
+    "url": "/comrades/static/media/pro (1).ff2ad42f.jpg"
+  },
+  {
+    "revision": "d5525f05f14792ca35523fef71d980ff",
+    "url": "/comrades/static/media/pro (8).d5525f05.jpg"
+  },
+  {
+    "revision": "febc697c8e84d9aa5916dd13a3b34966",
+    "url": "/comrades/static/media/whoarewe.febc697c.jpg"
+  },
+  {
+    "revision": "d38c83e35a57de521ce61f8f6edb9235",
+    "url": "/comrades/static/media/logo2.d38c83e3.png"
+  },
+  {
+    "revision": "c35188c33484943a51419141385ef6ca",
+    "url": "/comrades/static/media/img1.c35188c3.jpg"
+  },
+  {
+    "revision": "56629cd4c41a6921d016d9065f149eb2",
+    "url": "/comrades/static/media/Badge1.56629cd4.png"
+  },
+  {
+    "revision": "5f865e6141f434757edd4c9c4df06fd4",
+    "url": "/comrades/static/media/Badge2.5f865e61.png"
+  },
+  {
+    "revision": "116c4a2ce89e3a7303f37a6a1bcbb373",
+    "url": "/comrades/static/media/Badge3.116c4a2c.png"
+  },
+  {
+    "revision": "4649b0db130823c4cf6ed2f5a59470de",
+    "url": "/comrades/static/media/about2.4649b0db.jpg"
+  },
+  {
+    "revision": "91ad7758160612ebf18ab412da4d3f19",
+    "url": "/comrades/static/media/about3.91ad7758.jpg"
+  },
+  {
+    "revision": "5581d005a1cf549f4311b23c32a7e7a3",
+    "url": "/comrades/static/media/about4.5581d005.jpg"
+  },
+  {
+    "revision": "32a35ef08e4a01d1c9b78ff53e464855",
+    "url": "/comrades/static/media/CEO.32a35ef0.jpg"
+  },
+  {
+    "revision": "9a52f5cc2517fe95e5f363077a343066",
+    "url": "/comrades/static/media/executive.9a52f5cc.jpg"
+  },
+  {
+    "revision": "69ce5747bd847b88daabc0f0cf94763c",
+    "url": "/comrades/static/media/teams.69ce5747.jpg"
+  },
+  {
+    "revision": "c59da9ee8dbc680a74bd2b47b398051a",
+    "url": "/comrades/static/media/avt (1).c59da9ee.jpg"
+  },
+  {
+    "revision": "81445e4955f4242be4e9f34a8682d24b",
+    "url": "/comrades/static/media/avt (2).81445e49.jpg"
+  },
+  {
+    "revision": "e43e7a57e2bef9d531e8996013ff0e92",
+    "url": "/comrades/static/media/avt (3).e43e7a57.jpg"
+  },
+  {
+    "revision": "b1b5e26ac2736db74ec194419c6a499c",
+    "url": "/comrades/static/media/avt (4).b1b5e26a.jpg"
+  },
+  {
+    "revision": "3b8cb7844cc9861193cdf14fcb94c7b7",
+    "url": "/comrades/static/media/avt (5).3b8cb784.jpg"
+  },
+  {
+    "revision": "1a53da1d38aa1e9de6f1249b4aa341f7",
+    "url": "/comrades/static/media/avt (6).1a53da1d.jpg"
+  },
+  {
+    "revision": "2d08b40e661ee2399864f2ad24bd9ddb",
+    "url": "/comrades/static/media/avt (7).2d08b40e.jpg"
+  },
+  {
+    "revision": "6feee722fb7a2e9f609ec1c8b0566c03",
+    "url": "/comrades/static/media/avt (8).6feee722.jpg"
+  },
+  {
+    "revision": "d177e2c4b511c2496c3fc1ff95358ffc",
+    "url": "/comrades/static/media/HomeSlider.d177e2c4.jpg"
+  },
+  {
+    "revision": "954d1c90fa7957e81c2c0ba8da606770",
+    "url": "/comrades/index.html"
+  }
+];
