fa73721e5d18fc276c451324c08d4bf92088ab18
Hasham <hashamrehmat@gmail.com>
2022-04-14 08:57:41 +0500

    Updates

---
+++ precache-manifest.d1d1e152213be9049fd98ba9b5ce033a.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "480bd1a1743065f89e1a",
+    "url": "/static/css/main.97c66af9.chunk.css"
+  },
+  {
+    "revision": "480bd1a1743065f89e1a",
+    "url": "/static/js/main.480bd1a1.chunk.js"
+  },
+  {
+    "revision": "e0951f3e1edda8b7f479",
+    "url": "/static/js/1.e0951f3e.chunk.js"
+  },
+  {
+    "revision": "ea69a65924386940cf6d",
+    "url": "/static/js/2.ea69a659.chunk.js"
+  },
+  {
+    "revision": "6197523b7a637a52b588",
+    "url": "/static/js/runtime~main.6197523b.js"
+  },
+  {
+    "revision": "d33d35027efea781cb28e51a62dff456",
+    "url": "/static/media/HomeSlider22.d33d3502.jpg"
+  },
+  {
+    "revision": "0876dd7c0d046ecf63cef4302ac0f687",
+    "url": "/static/media/services-bg.0876dd7c.jpg"
+  },
+  {
+    "revision": "7b1cedd0f4ee2b04cd1cd82bbcb860cd",
+    "url": "/static/media/counter-bg.7b1cedd0.jpg"
+  },
+  {
+    "revision": "0b452413f60facecb10624c543a0e72b",
+    "url": "/static/media/company.0b452413.png"
+  },
+  {
+    "revision": "450f54c36d05ec1fd9954b4d74818466",
+    "url": "/static/media/contact-bg.450f54c3.jpg"
+  },
+  {
+    "revision": "dd0bd974be9072c390d576642ab753ad",
+    "url": "/static/media/portfolio-bg.dd0bd974.jpg"
+  },
+  {
+    "revision": "603d5fe753a0c7b6722a4b1a6df35cb0",
+    "url": "/static/media/service-bg.603d5fe7.jpg"
+  },
+  {
+    "revision": "db6b63cbbc08019b524dda3d89201697",
+    "url": "/static/media/team-bg.db6b63cb.jpg"
+  },
+  {
+    "revision": "ee71f72ccdfa4205cf2bfa17e4eef433",
+    "url": "/static/media/about-bg.ee71f72c.jpg"
+  },
+  {
+    "revision": "cf2090ba78e7aa7e35aecb031d3ba6e3",
+    "url": "/static/media/service1.cf2090ba.jpg"
+  },
+  {
+    "revision": "8cc147a01d1106872de353b06dcad205",
+    "url": "/static/media/service2.8cc147a0.jpg"
+  },
+  {
+    "revision": "6da1724f65c3ab7513d5721c7423ab89",
+    "url": "/static/media/service3.6da1724f.jpg"
+  },
+  {
+    "revision": "3cd5092c019ee24588d307fa44c0e7cf",
+    "url": "/static/media/service4.3cd5092c.jpg"
+  },
+  {
+    "revision": "79111267edae3f4e71bdb7965c65995f",
+    "url": "/static/media/pro (2).79111267.jpg"
+  },
+  {
+    "revision": "f9f901e2ac80de2a2eea194388a671e7",
+    "url": "/static/media/pro (3).f9f901e2.jpg"
+  },
+  {
+    "revision": "badf757da3de6e6a888c33c0cddfd4b0",
+    "url": "/static/media/pro (4).badf757d.jpg"
+  },
+  {
+    "revision": "2ce96713c23b9f08429bfa73156eb11a",
+    "url": "/static/media/pro (5).2ce96713.jpg"
+  },
+  {
+    "revision": "ef298bd85d125d9425db4a7b821cd12d",
+    "url": "/static/media/pro (6).ef298bd8.jpg"
+  },
+  {
+    "revision": "4149c5c5f1f6606082cc880906260f88",
+    "url": "/static/media/pro (7).4149c5c5.jpg"
+  },
+  {
+    "revision": "e690a46bf07bf35bdc08d6c6b797620c",
+    "url": "/static/media/logo.e690a46b.png"
+  },
+  {
+    "revision": "fc7a02bfd86ebdbca8d1d5524d6aecc0",
+    "url": "/static/media/about1.fc7a02bf.jpg"
+  },
+  {
+    "revision": "ff2ad42f1649c87a675a04c7ac7828d6",
+    "url": "/static/media/pro (1).ff2ad42f.jpg"
+  },
+  {
+    "revision": "d5525f05f14792ca35523fef71d980ff",
+    "url": "/static/media/pro (8).d5525f05.jpg"
+  },
+  {
+    "revision": "febc697c8e84d9aa5916dd13a3b34966",
+    "url": "/static/media/whoarewe.febc697c.jpg"
+  },
+  {
+    "revision": "d38c83e35a57de521ce61f8f6edb9235",
+    "url": "/static/media/logo2.d38c83e3.png"
+  },
+  {
+    "revision": "c35188c33484943a51419141385ef6ca",
+    "url": "/static/media/img1.c35188c3.jpg"
+  },
+  {
+    "revision": "56629cd4c41a6921d016d9065f149eb2",
+    "url": "/static/media/Badge1.56629cd4.png"
+  },
+  {
+    "revision": "5f865e6141f434757edd4c9c4df06fd4",
+    "url": "/static/media/Badge2.5f865e61.png"
+  },
+  {
+    "revision": "116c4a2ce89e3a7303f37a6a1bcbb373",
+    "url": "/static/media/Badge3.116c4a2c.png"
+  },
+  {
+    "revision": "4649b0db130823c4cf6ed2f5a59470de",
+    "url": "/static/media/about2.4649b0db.jpg"
+  },
+  {
+    "revision": "91ad7758160612ebf18ab412da4d3f19",
+    "url": "/static/media/about3.91ad7758.jpg"
+  },
+  {
+    "revision": "5581d005a1cf549f4311b23c32a7e7a3",
+    "url": "/static/media/about4.5581d005.jpg"
+  },
+  {
+    "revision": "32a35ef08e4a01d1c9b78ff53e464855",
+    "url": "/static/media/CEO.32a35ef0.jpg"
+  },
+  {
+    "revision": "9a52f5cc2517fe95e5f363077a343066",
+    "url": "/static/media/executive.9a52f5cc.jpg"
+  },
+  {
+    "revision": "69ce5747bd847b88daabc0f0cf94763c",
+    "url": "/static/media/teams.69ce5747.jpg"
+  },
+  {
+    "revision": "c59da9ee8dbc680a74bd2b47b398051a",
+    "url": "/static/media/avt (1).c59da9ee.jpg"
+  },
+  {
+    "revision": "81445e4955f4242be4e9f34a8682d24b",
+    "url": "/static/media/avt (2).81445e49.jpg"
+  },
+  {
+    "revision": "e43e7a57e2bef9d531e8996013ff0e92",
+    "url": "/static/media/avt (3).e43e7a57.jpg"
+  },
+  {
+    "revision": "b1b5e26ac2736db74ec194419c6a499c",
+    "url": "/static/media/avt (4).b1b5e26a.jpg"
+  },
+  {
+    "revision": "3b8cb7844cc9861193cdf14fcb94c7b7",
+    "url": "/static/media/avt (5).3b8cb784.jpg"
+  },
+  {
+    "revision": "1a53da1d38aa1e9de6f1249b4aa341f7",
+    "url": "/static/media/avt (6).1a53da1d.jpg"
+  },
+  {
+    "revision": "2d08b40e661ee2399864f2ad24bd9ddb",
+    "url": "/static/media/avt (7).2d08b40e.jpg"
+  },
+  {
+    "revision": "6feee722fb7a2e9f609ec1c8b0566c03",
+    "url": "/static/media/avt (8).6feee722.jpg"
+  },
+  {
+    "revision": "d177e2c4b511c2496c3fc1ff95358ffc",
+    "url": "/static/media/HomeSlider.d177e2c4.jpg"
+  },
+  {
+    "revision": "66086411fb3e9d0180a2ed490a912f8c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/comrades/precache-manifest.173d3032eb8890b2aae04856d38c143b.js"
+  "/precache-manifest.d1d1e152213be9049fd98ba9b5ce033a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/comrades/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
